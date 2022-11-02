2a2cd291183807cc2d9de607b0cb50c266ea0487
ngocyen0709 <yenb1906811@student.ctu.edu.vn>
2022-11-02 17:03:13 +0700

    Tao trang cap nhat lien he

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <script>
 import AppHeader from "@/components/AppHeader.vue";
-
-export default {
-    components: {
+export default{
+  components: {
     AppHeader,
-    },
-};
+  },
+}
 </script>
 
 <template>
-    <div id="app">
-        <AppHeader />
-        <div class="container mt-3">
-            <router-view />
-        </div>
-    </div>
+ <div id="app">
+<AppHeader />
+<div class="container mt-3">
+<router-view />
+</div>
+</div>
+
 </template>
 
 <style>
 .page {
-    max-width: 400px;
-    margin: auto;
+  max-width: 400px;
+  margin: auto;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,12 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
-    // proxy: {
-    // "/api": {
-    // target: "http://localhost:3031/",
-    // changeOrigin: true,
-    // },
-    // }
-    },
+    port: 3001,
+    proxy: {
+        "/api": {
+            target: "http://localhost:3000/",
+            changeOrigin: true,
+          },
+    }
+},
 });
